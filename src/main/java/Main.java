04b091ea879572ff26c7fd7469a9e3d0bb691c6e
Nada-Hany <nadaaayman993@gmail.com>
2023-12-02 03:20:10 +0200

    main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.example.watch_it.*;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("data.txt");
+        Admin.readFile(file);
+    }
+
+}
