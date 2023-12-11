82e695408bacea68acaa11f6324948f27c43b5e1
Youssef Mohsen <youssefkimo9@gmail.com>
2023-12-11 15:59:59 +0200

    Update movie-view(Rate)

--- src/main/java/com/example/watch_it/MovieController.java
+++ src/main/java/com/example/watch_it/MovieController.java
@@ -10,9 +10,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 
 public class MovieController {
@@ -34,6 +36,26 @@ public class MovieController {
     private ImageView imagePreview;
     @FXML
     private Button Back;
+    @FXML
+    private Button Watch;
+
+    @FXML
+    private ImageView star1;
+
+    @FXML
+    private ImageView star2;
+
+    @FXML
+    private ImageView star3;
+
+    @FXML
+    private ImageView star4;
+
+    @FXML
+    private ImageView star5;
+
+    private ImageView[] stars;
+
 
     // Other fields and methods...
 
@@ -42,10 +64,62 @@ public class MovieController {
 
     @FXML
     private void initialize() {
+        stars = new ImageView[]{star1, star2, star3, star4, star5};
+        // Set empty star images
+        for (ImageView star : stars) {
+            star.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/emptyStar.png"))));
+        }
         onMouseEntered();
         onMouseExit();
     }
+    @FXML
+    private boolean handleStarClick(MouseEvent event) {
+        ImageView clickedStar = (ImageView) event.getSource();
+        int rating = Integer.parseInt(clickedStar.getId().substring(4)); // Extract the rating from the star's ID
+        for (int i = 0; i < rating; i++) {
+            int finalI1 = i;
+            stars[i].setOnMouseClicked(event1 -> {
+                ImageView clickedStar2 = (ImageView) event1.getSource();
+                int rating2 = Integer.parseInt(clickedStar2.getId().substring(4)); // Extract the rating from the star's ID
+                System.out.println("Rated: " + rating2 + " stars");
+                stars[finalI1].setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/fullStar.png"))));
+                for (int j = 0; j < rating2+(5-rating2); j++){
+                    int finalJ = j;
+                    stars[j].setOnMouseExited(event2 ->
+                            stars[finalJ].setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/fullStar.png")))));
+                    }
+                for (int h = rating2+(4-rating2); h >= rating2; h--){
+                    int finalJ = h;
+                    stars[h].setOnMouseExited(event2 ->
+                            stars[finalJ].setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/emptyStar.png")))));
+                }
+            });
 
+        }
+        // You can perform additional actions here, such as saving the rating to a database.
+        return false;
+    }
+
+    @FXML
+    private void handleStarHover(MouseEvent event) {
+        ImageView hoveredStar = (ImageView) event.getSource();
+        int rating = Integer.parseInt(hoveredStar.getId().substring(4)); // Extract the rating from the star's ID
+        // Highlight stars
+
+            for (int i = 0; i < rating; i++) {
+                stars[i].setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/fullStar.png"))));
+            }
+
+    }
+
+    @FXML
+    private void resetStars(MouseEvent event) {
+        if(!handleStarClick(event)) {
+            for (ImageView star : stars) {
+                star.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("assets/emptyStar.png"))));
+            }
+        }
+    }
     public void refreshScreen(String filmTitle, String newMovieName, String newFilm, String filmGenre,
                               String filmDescription, String filmDuration, Image image) {
         titleMovie.setText(filmTitle);
@@ -85,10 +159,14 @@ public class MovieController {
 
     public void onMouseEntered() {
         Back.setOnMouseEntered(event -> Back.setStyle("-fx-background-color: #FFC107; -fx-background-radius: 25; -fx-border-color: white; -fx-border-radius: 25;"));
+        Watch.setOnMouseEntered(event -> Watch.setStyle("-fx-background-color: #FFC107; -fx-background-radius: 25; -fx-border-color: white; -fx-border-radius: 25;"));
+
+
     }
 
     public void onMouseExit() {
         Back.setOnMouseExited(event -> Back.setStyle("-fx-background-color: black; -fx-background-radius: 25; -fx-border-color: white; -fx-border-radius: 25;"));
+        Watch.setOnMouseExited(event -> Watch.setStyle("-fx-background-color: black; -fx-background-radius: 25; -fx-border-color: white; -fx-border-radius: 25;"));
     }
     //////////////////////////
 
